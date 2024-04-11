2ce1ee93ea50c01a2d58e4dc79aea02cea959e3f
“suyashkhanzode” <suyashkhanzode123@gmail.com>
2024-04-11 22:49:54 +0530

    Request/Response Assignment

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const http = require('http');
+const http = require("http");
 
-let server = http.createServer((req,res)=>{
-      console.log("Suyash Khanzode")
-})
+let server = http.createServer((req, res) => {
+  let url = req.url;
+
+  if (url == "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><head>");
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url == "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url == "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+});
 
 server.listen(4000);
-
